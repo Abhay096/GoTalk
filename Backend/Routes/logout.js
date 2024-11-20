dade0fecbdf7e3abd335463ff3debef605754c11
Abhay096 <yuno88006@gmail.com>
2024-11-20 12:15:02 +0530

    .

--- Backend/Routes/logout.js
+++ Backend/Routes/logout.js
@@ -2,10 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/logout', (req, res) => {
-    const userPhoneNumber = req.body.userPhoneNumber
     // Clear the token cookie by its name ('token' in this case)
-    console.log(userPhoneNumber);
-    return 0;
     res.clearCookie('token', {
         httpOnly: true,
         secure: true, // Same options used when setting the cookie
